d37bc79cc7d38568efe4d31503d90407f87e0b3a
Santiago Basulto <santiago.basulto@gmail.com>
2018-12-06 20:42:22 -0300

    admin + user

--- sample_project/sample_app/admin.py
+++ sample_project/sample_app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Country, Author, Book
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
